85ffd4a3b2b4d55f2ea6996ad07065038ccf4232
Aste <aste@tuta.io>
2023-08-08 15:20:55 +0200

    Add Webpack configuration and modules, promise refactor and initial styling

--- src/index.js
+++ src/index.js
@@ -1,50 +1,81 @@
-// const dateFns = require('date-fns');
+import './styles.css'
+import { format } from 'date-fns'
 
 async function getForecast(location) {
-    const weatherServerResponse = await fetch(
-        `http://api.weatherapi.com/v1/forecast.json?key=b850ee2d91154e8b913155353232806&q=${location}`,
-        { mode: "cors" }
-    )
-    // const weatherData = await weatherServerResponse
-    const weatherData = await weatherServerResponse.json()
-    console.log(weatherData)
+    let weatherData
 
-    let cityName = weatherData.location.name
-    let countryName = weatherData.location.country
+    try {
+        const apiKey = 'b850ee2d91154e8b913155353232806'
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`, { mode: 'cors' })
 
-    let currentConditionShortText = weatherData.current.condition.text
-    let currentConditionIcon = weatherData.current.condition.icon
-    let currentTempC = Math.round(weatherData.current.temp_c)
-    let currentPercipMm = weatherData.current.percip_mm
-    let currentWindKph = weatherData.current.wind_kph
-    let currentWindDirection = weatherData.current.wind_degree
-    let currentHumidity = weatherData.current.humidity
-    let currentUvIndex = weatherData.current.uv
-    let currentTimeEpoc = weatherData.current.time_epoch
+        console.log(response)
 
-    let todaysSunriseTime = weatherData.forecast.forecastday[0].astro.sunrise
-    let todaysSunsetTime = weatherData.forecast.forecastday[0].astro.sunset
-    let todaysChanceOfRain = weatherData.forecast.forecastday[0].day.daily_chance_of_rain
-    let todaysMaxTempC = Math.round(weatherData.forecast.forecastday[0].day.maxtemp_c)
-    let todaysMinTempC = Math.round(weatherData.forecast.forecastday[0].day.mintemp_c)
+        weatherData = await response.json()
 
-    const function getHourlyForecast(currentTimeEpoc) {
+        console.log(weatherData)
+        let cityName = weatherData.location.name
+        let countryName = weatherData.location.country
+
+        let currentConditionText = weatherData.current.condition.text
+        let currentConditionIcon = weatherData.current.condition.icon
+        let currentTempC = Math.round(weatherData.current.temp_c)
+        let currentPercipMm = weatherData.current.percip_mm
+        let currentWindKph = weatherData.current.wind_kph
+        let currentWindDirection = weatherData.current.wind_degree
+        let currentHumidity = weatherData.current.humidity
+        let currentUvIndex = weatherData.current.uv
+        let currentTimeEpoc = weatherData.current.time_epoch
+
+        let todaysSunriseTime = weatherData.forecast.forecastday[0].astro.sunrise
+        let todaysSunsetTime = weatherData.forecast.forecastday[0].astro.sunset
+        let todaysChanceOfRain = weatherData.forecast.forecastday[0].day.daily_chance_of_rain
+        let todaysMaxTempC = Math.round(weatherData.forecast.forecastday[0].day.maxtemp_c)
+        let todaysMinTempC = Math.round(weatherData.forecast.forecastday[0].day.mintemp_c)
+
+        document.getElementById('cityName').innerHTML = cityName
+        document.getElementById('country').innerHTML = countryName
+        document.getElementById('currentConditionText').innerHTML = currentConditionText
+        // document.getElementById('currentConditionIcon').innerHTML = currentConditionIcon
+        document.getElementById('todaysMaxTemp').innerHTML = `H:${todaysMaxTempC}°`
+        document.getElementById('todaysMinTemp').innerHTML = `L:${todaysMinTempC}°`
+        document.getElementById('currentTemp').innerHTML = `${currentTempC}°`
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+
+
+    } catch (error) {
+        console.error("There was a problem fetching the weather data", error)
+        return
+    }
+
+
+
+
+
+    function createHourlyForecastElements(currentTimeEpoc) {
         // reference the parent element
         // generate a for loop
         // get the necessary data from the the api for each hour using each hour of the day in the for loop
         // create the elements with the data inside
+        // time, weather icon, temperature
+        // append the element to the parent, continue until all hours are made
+    }
+
+
+    function generate3dayForecastElements(currentTimeEpoc) {
+        // reference the parent element
+        // generate a for loop
+        // get the necessary data from the the api for each hour using each hour of the day in the for loop
+        // create the elements with the data inside
+        // time, weather icon, temperature
         // append the element to the parent, continue until all hours are made
     }
 
 
 
-    document.getElementById('cityName').innerHTML = cityName
-    document.getElementById('country').innerHTML = countryName
-    document.getElementById('currentConditionShort').innerHTML = currentConditionShortText
-    document.getElementById('currentConditionShort').innerHTML = currentConditionIcon
-    document.getElementById('todaysMaxTemp').innerHTML = `H:${todaysMaxTempC}°`
-    document.getElementById('todaysMinTemp').innerHTML = `L:${todaysMinTempC}°`
-    document.getElementById('currentTemp').innerHTML = `${currentTempC}°`
 }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
-module.export = {
+module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 8080,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
 }
