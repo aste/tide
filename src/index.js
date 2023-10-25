59ade2233da4d784392b89d09ac02b46fc1a1f41
Aste <aste@tuta.io>
2023-10-25 16:11:55 +0200

    Add cloud gradient functionality and minor sky color tuning

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import "./styles.css";
-import { addHours, format } from "date-fns";
+import { fromUnixTime, addHours, format } from "date-fns";
 
 const SunCalc = require('suncalc');
 const locationForm = document.getElementById("locationForm")
 
-let location = "tokyo"
-let lat
-let lon
+let location = "Tijuana"
+
 
 async function getForecast(location) {
   let weatherData;
 
   try {
     const apiKey = "b850ee2d91154e8b913155353232806";
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`
-    );
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`
+
+    const response = await fetch(apiUrl);
 
     weatherData = await response.json();
 
@@ -27,38 +26,15 @@ async function getForecast(location) {
       { mode: "cors" }
     }
 
-    // const currentHour = new Date(weatherData.forecast.forecastday[0].date);
-    let cityName = weatherData.location.name;
-    let countryName = weatherData.location.country;
-    lat = weatherData.location.lat
-    lon = weatherData.location.lon
-    // currentTime = weatherData.current.last_updated_epoch
+    console.log(weatherData)
+
+    setBackgroundSkyGradient(weatherData)
+
+    setCurrentConditions(weatherData)
+
+    // setHourlyForecast(weatherData)
 
 
-    let currentConditionText = convertToTitleCase(String(weatherData.current.condition.text));
-    let currentConditionIcon = weatherData.current.condition.icon;
-    let currentTempC = Math.round(weatherData.current.temp_c);
-    let currentPercipMm = weatherData.current.percip_mm;
-    let currentWindKph = weatherData.current.wind_kph;
-    let currentWindDirection = weatherData.current.wind_degree;
-    let currentHumidity = weatherData.current.humidity;
-    let currentUvIndex = weatherData.current.uv;
-    let currentTimeEpoc = weatherData.current.time_epoch;
-    let todaysSunriseTime = weatherData.forecast.forecastday[0].astro.sunrise;
-    let todaysSunsetTime = weatherData.forecast.forecastday[0].astro.sunset;
-    let todaysChanceOfRain = weatherData.forecast.forecastday[0].day.daily_chance_of_rain;
-    let todaysMaxTempC = Math.round(weatherData.forecast.forecastday[0].day.maxtemp_c);
-    let todaysMinTempC = Math.round(weatherData.forecast.forecastday[0].day.mintemp_c);
-
-    document.getElementById("locationName").innerHTML = cityName;
-    document.getElementById("country").innerHTML = countryName;
-    document.getElementById("currentConditionShort").innerHTML = currentConditionText;
-    // document.getElementById('currentConditionIcon').src = currentConditionIcon
-    document.getElementById("todaysMaxTemp").innerHTML = `H:${todaysMaxTempC}°`;
-    document.getElementById("todaysMinTemp").innerHTML = `L:${todaysMinTempC}°`;
-    document.getElementById("currentTemp").innerHTML = `${currentTempC}°`;
-
-    setSkyGradient()
     return true
 
   } catch (error) {
@@ -66,21 +42,30 @@ async function getForecast(location) {
     return false;
   }
 
-  // getHourlyForecast(currentHour);
-
-  console.log(weatherData);
-  // get3DayForecast();
 }
 
-function convertToTitleCase(str) {
-  let splitStr = str.toLowerCase().split(' ')
-  for (let i = 0; i < splitStr.length; i++) {
-    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-  }
-  return splitStr.join(' ')
+function getCurrentLocalHour() {
+
 }
 
-function getHourlyForecast(currentHour) {
+function setCurrentConditions(weatherData) {
+  const cityName = weatherData.location.name;
+  const countryName = weatherData.location.country;
+  const currentConditionText = convertToTitleCase(String(weatherData.current.condition.text));
+  const currentTempC = Math.round(weatherData.current.temp_c);
+  const todaysMaxTempC = Math.round(weatherData.forecast.forecastday[0].day.maxtemp_c);
+  const todaysMinTempC = Math.round(weatherData.forecast.forecastday[0].day.mintemp_c);
+
+  document.getElementById("locationName").innerHTML = cityName;
+  document.getElementById("country").innerHTML = `${countryName}`;
+  document.getElementById("currentTemp").innerHTML = `${currentTempC}°`;
+  document.getElementById("currentConditionShort").innerHTML = currentConditionText;
+  document.getElementById("todaysMaxTemp").innerHTML = `H:${todaysMaxTempC}°`;
+  document.getElementById("todaysMinTemp").innerHTML = `L:${todaysMinTempC}°`;
+}
+
+function setHourlyForecast(weatherData) {
+  console.log(weatherData)
   const parentElement = document.getElementById("hourlyForecast");
 
   for (let i = 0; i < 24; i++) {
@@ -90,22 +75,14 @@ function getHourlyForecast(currentHour) {
     } else {
       forecastHour = format(addHours(currentHour, i), "ha");
     }
-    // console.log(forecastHour);
-    // console.log(weatherData.forecast.forecastday[0].hour[i].condition.icon);
-    // console.log(`${Math.round(weatherData.forecast.forecastday[0].hour[i].temp_c)}°`);
-    // console.log("");
-
-    //<div class="weather-info">
-    //<img src="path-to-icon.png" alt="Weather Icon" class="weather-icon" />
-    //<span class="temperature">72°F</span>
-    //<time class="time">9 AM</time>
-    // </div>
+    const currentLocalTime = format(new Date(weatherData.location.localtime), "H:mm aaaaa'm'")
+    document.getElementById("currentLocalTime").innerHTML = `${currentLocalTime}`;
   }
-  // create the elements with the data inside
-  // append the element to the parent, continue until all hours are made
 }
 
-function get3DayForecast() {
+
+
+function setThreeDayForecast(weatherData) {
   const parentElement = document.getElementById("weeklyForecast");
 
   for (let i = 0; i < 3; i++) {
@@ -113,7 +90,7 @@ function get3DayForecast() {
     if (i === 0) {
       day = "Today";
     } else {
-      // day = allWeekdays[getDay(new Date(weatherData.forecast.forecastday[i].date))];
+      day = allWeekdays[getDay(new Date(weatherData.forecast.forecastday[i].date))];
     }
     console.log(day);
     console.log(weatherData.forecast.forecastday[i].day.condition.icon);
@@ -122,25 +99,40 @@ function get3DayForecast() {
     console.log("");
   }
 
-  // create the elements with the data inside
-  // append the element to the parent, continue until all days are made
 }
 
-function setSkyGradient() {
+function setBackgroundSkyGradient(weatherData) {
+  const lat = weatherData.location.lat
+  const lon = weatherData.location.lon
   const currentTime = new Date()
   const sunTimes = SunCalc.getTimes(currentTime, lat, lon);
+  const cloudCover = weatherData.current.cloud
+  const cloudOpacity = cloudCover / 100
 
-  let gradient;
+  let skyGradient
+  let cloudGradient
 
   if (currentTime > sunTimes.sunrise && currentTime < sunTimes.sunset) {
-    gradient = "linear-gradient(to top, #89CFF0, #3ca0ff)"; // Daytime
+    skyGradient = "linear-gradient(to top, #89CFF0, #3ca0ff 50%, #3ca0ff)";
+    cloudGradient = `linear-gradient(to top, rgba(0, 0, 0, 0), rgba(255, 255, 255, ${cloudOpacity * 0.75}) 50%, rgba(255, 255, 255, ${cloudOpacity * 1.5}) 95% )`
   } else if (currentTime > sunTimes.dawn && currentTime < sunTimes.dusk) {
-    gradient = "linear-gradient(to top, #ffb100, #89CFF0)"; // Sunrise/Sunset
+    skyGradient = "linear-gradient(to top, #ffb100, #89CFF0 30%)";
+    cloudGradient = `linear-gradient(to top, rgba(0, 0, 0, 0),  rgba(255, 255, 255, ${cloudOpacity}))`
   } else {
-    gradient = "linear-gradient(to top, #0c0c3c, #000000)"; // Nighttime
+    skyGradient = "linear-gradient(to top, #0c0c3c, #000000 50%, #000000)";
+    cloudGradient = `linear-gradient(to top, rgba(0, 0, 0, 0), rgba(255, 255, 255, ${cloudOpacity * 0.35}) 50%, rgba(255, 255, 255, ${cloudOpacity * 0.75}) 100% )`
   }
 
-  document.body.style.backgroundImage = `${gradient}`;
+  document.body.style.backgroundImage = `${cloudGradient}, ${skyGradient}`;
+}
+
+
+function convertToTitleCase(str) {
+  let splitStr = str.toLowerCase().split(' ')
+  for (let i = 0; i < splitStr.length; i++) {
+    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  return splitStr.join(' ')
 }
 
 locationForm.addEventListener('submit', function (e) {
